ae5182abc7f5662faa832c6d6e5e737999dea750
churchgh <gabechurch07@gmail.com>
2022-04-26 12:27:04 -0400

    First commit

---
+++ deck_of_cards.py
@@ -0,0 +1,21 @@
+import requests
+
+response = requests.get('https://deckofcardsapi.com/api/deck/new/')
+
+if response.status_code == 200:
+    deck_id = response.json()['deck_id']
+    requests.get(f"https://deckofcardsapi.com/api/deck/{deck_id}/shuffle/")
+else:
+    print(f"Request unsuccessful. Status code: {response.status_code}")
+    exit()
+
+draw_response = requests.get(f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5")
+
+draw = draw_response.json()
+#print("\nDraw: \n", draw)
+
+i = 1
+
+for card in draw['cards']:
+    print(f"Card {i} is {card['value']} of {card['suit']}")
+    i +=1
